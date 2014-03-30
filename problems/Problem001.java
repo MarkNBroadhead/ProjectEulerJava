46b73185d0009f69d7ccd6c13137bcfd35da80e1
Mark Broadhead <orcslayermack@gmail.com>
2014-03-29 18:51:28 -0600

    Added more documentation and cleaned up the code.

--- problems/Problem001.java
+++ problems/Problem001.java
@@ -6,6 +6,7 @@ package problems;
 import java.util.*;
 
 /**
+ * This class solves Problem 1 from ProjectEuler.net
  * @author Mark N Broadhead
  * @version 29/3/2014
  * 
@@ -21,19 +22,22 @@ public class Problem001 {
 		Integer limit = 1000;
 
 		int sum1 = algorithm1(number1, number2, limit);
-		//int sum2 = algorithm2(number1, number2, limit);
 
 		System.out.println();
 		System.out.println("The sum of the multiples of " + number1 + " and " + number2 + " is: " + sum1);
-		//System.out.println("The sum of the multiples of " + number1 + " and " + number2 + " is: " + sum2);
-
-		
 
 		long endTime = System.currentTimeMillis();
 		System.out.println("Total execution time: " + (endTime - startTime) );
 	}
 	
-	public static int algorithm1(int number1, int number2, int limit){
+	/**
+	 * 
+	 * @param number1 One multiple to calculate
+	 * @param number2 Second multiple to calculate
+	 * @param limit Calculate all multiples up to this number
+	 * @return The sum of all multiples of number1 and number2 up to limit
+	 */
+		public static int algorithm1(int number1, int number2, int limit){
 		// populate a list with all the multiples of number1 up to limit
 		List<Integer> jointList = new ArrayList<Integer>(multiples(number1, limit));
 		// populate a second list with multiples of number2 up to limit
@@ -51,6 +55,13 @@ public class Problem001 {
 		}
 		return sum;
 	}
+	
+	/**
+	 * 
+	 * @param seed calculate the multiples of this number
+	 * @param limit will calculate all multiples up to this point
+	 * @return a list of all the multiples of seed up to the limit
+	 */
 	public static List<Integer> multiples(Integer seed, Integer limit) {
 		List<Integer> a = new ArrayList<Integer>();
 		for ( int i = 0; seed * i < limit; i++) {
@@ -58,5 +69,4 @@ public class Problem001 {
 		}
 		return a;
 	}
-
 }
